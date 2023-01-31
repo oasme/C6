0c4a023646d63a660b7b5fc7508da67cb2b94862
oasme <102040125+oasme@users.noreply.github.com>
2023-01-31 14:52:11 +0800

    Create 01_01.c

---
+++ ch01/01_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int dogs;
+    printf("How many dogs do you have?\n");
+    scanf("%d",&dogs);
+    printf("So you have %d dos(s)!\n",dogs);
+
+    return 0;
+
+}
